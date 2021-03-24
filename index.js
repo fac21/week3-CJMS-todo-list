1cc0e9046a669d135bb3eee12e784f271fd8afab
Saki Reid <saki.reid@gmail.com>
2021-03-24 16:17:05 +0100

    created test for add list functionality
    Relates #2

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
+
 const addButton = document.querySelector("#add-button");
+const newTodo = document.getElementById("new-to-do");
+const taskList = document.getElementById("task-list");
+
 addButton.addEventListener("click", (event) => {
     event.preventDefault();
     submitTask();
 });
 
-
-const newTodo = document.getElementById("new-to-do");
-const taskList = document.getElementById("task-list");
-
 function submitTask() {
     let newItem = document.createElement("li");
-    taskList.append(newItem);
     newItem.innerText = newTodo.value;
+    taskList.append(newItem);
 }
