669c2fa2350a61b5c0250e0a8a1fe2a8fc0f234d
chunzg <chunzhang53@gmail.com>
2021-03-24 17:33:53 +0000

    Start writing delete functionality

--- index.js
+++ index.js
@@ -2,7 +2,8 @@
 const addButton = document.querySelector("#add-button");
 const newTodo = document.getElementById("new-to-do");
 const taskList = document.getElementById("task-list");
-
+const deleteBtns = document.querySelectorAll(".deleteBtn");
+console.log(deleteBtns)
 addButton.addEventListener("click", (event) => {
     event.preventDefault();
     submitTask();
@@ -11,8 +12,24 @@ addButton.addEventListener("click", (event) => {
 function submitTask() {
     let temp = document.getElementsByTagName("template")[0];
     let clone = temp.content.cloneNode(true);
-    clone.children[1].innerText = newTodo.value;
+    clone.querySelector("label").textContent = newTodo.value;
+    // clone.children[1].innerText = newTodo.value;
     taskList.appendChild(clone);
+}
+
+deleteBtns.forEach(x => {
+    console.log('hello')
+    x.addEventListener("click", (event) => {
+        event.preventDefault()
+        deleteTask(x)
+    })
+})
+
+function deleteTask(btn) {
+    console.log(btn)
+    //find the corresponding article 
+    // btn.parentNode.remove()
+    //and delete
 }
 
 
--- index.test.js
+++ index.test.js
@@ -1,14 +1,14 @@
-test("Submitting a new task adds it to the list", () => {
-  const input = document.querySelector("input");
-  input.value = "test";
-  const submitButton = document.querySelector("button[type='submit']");
-  submitButton.click(); 
-  const listItem = document.querySelector("li");
-  if (listItem){
-    equal(listItem.textContent, input.value)
-  }
-  input.textContent = "";
-});
+// test("Submitting a new task adds it to the list", () => {
+//   const input = document.querySelector("input");
+//   input.value = "test";
+//   const submitButton = document.querySelector("button[type='submit']");
+//   submitButton.click(); 
+//   const listItem = document.querySelector("li");
+//   if (listItem){
+//     equal(listItem.textContent, input.value)
+//   }
+//   input.textContent = "";
+// });
 
   // test("Checking an entry marks it as complete", () => {
   //   // test goes here
